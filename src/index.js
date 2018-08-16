fd9353feaf62af31a6832cb04d2beaeca0057916
mattperkins <github@mattperkins.com>
2018-08-16 21:47:51 +0100

    add data to UI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import styled, {injectGlobal} from 'styled-components'
+import axios from 'axios'
 
 //eslint-disable-next-line
 injectGlobal`
@@ -19,25 +20,34 @@ margin: 100px;
 
 // main ("ROOT") component 
 export default class Root extends React.Component {
-
-
-// MAIN COMPONENT RENDER
-render() {
-this.componentDidMount(){
+  state={
+    posts: []
+  }
+componentDidMount(){
   axios.get("https://www.reddit.com/r/patterns.json")
   .then(res => {
-    console.log(res.data.data.children)
+    this.setState({
+      posts: res.data.data.children
+    })
   })
   .catch(err => {
     console.log(err)
   })
 }
+// MAIN COMPONENT RENDER
+render() {
+
 // MAIN COMPONENT RETURN
 return (
 
 <Wrapper>
 
-<h1>Hello, World!</h1>
+{this.state.posts.map((item,i) => 
+  <div key={i}>
+    <img src={item.data.thumbnail} />
+    <p>{item.data.title}</p>
+  </div>
+)}
 
 </Wrapper>
 
